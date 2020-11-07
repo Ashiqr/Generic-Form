815019c43e01d1ae2c6d39088d39dc55fd98c34a
Ashiq <asramlucken@gmail.com>
2020-11-07 15:11:09 +0200

    first commit

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+// reusable form component goes here
+// should be generic, with no code related to one specific type of form
+// see the REUSABLE COMPONENT section for details
+import React from 'react';
+import Name from './Name';
+
+const Form = () => <sub>
+    <Name/>
+</sub>
+
+export default Form
---
+++ src/Components/Name.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Name = () => {return (
+    <React.Fragment>
+        <span>Name:</span>
+        <input type='text' placeholder='Name'></input> 
+    </React.Fragment>
+    )};
+
+export default Name;
